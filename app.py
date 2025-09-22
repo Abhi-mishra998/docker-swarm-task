6490eaeb4ab0c1a8b55484c0378f7acddcd2d7b7
kali <kali@kali.kali>
2025-09-22 13:00:14 -0400

    containerize a simple Python Flask API and serve it behind an Nginx reverse proxy

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify
+import socket
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"msg": "Hello from backend Make by Abhishek Mishra just for testing"})
+    return jsonify({
+        "msg": "Welcome to the backend by Abhishek Mishra",
+        "host": socket.gethostname()
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
