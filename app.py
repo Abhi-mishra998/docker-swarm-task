0785e974d410934bcbda3e382bfca884c8614685
kali <kali@kali.kali>
2025-09-22 09:48:19 -0400

    write the simple docker file to run the backend

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"msg": "Hello from backend"})
+    return jsonify({"msg": "Hello from backend Make by Abhishek Mishra just for testing"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
